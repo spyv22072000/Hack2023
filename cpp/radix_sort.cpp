39d291ee119909180be3b513da893ab053ecca9b
Sandeep Yadav <78311132+spyv22072000@users.noreply.github.com>
2023-10-16 10:06:02 +0530

    Create radix_sort.cpp

---
+++ cpp/radix_sort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void countingSort(vector<int>& arr, int exp) {
+    int n = arr.size();
+    vector<int> output(n);
+    vector<int> count(10, 0);
+
+    for (int i = 0; i < n; i++) {
+        int index = (arr[i] / exp) % 10;
+        count[index]++;
+    }
+
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        int index = (arr[i] / exp) % 10;
+        output[count[index] - 1] = arr[i];
+        count[index]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void radixSort(vector<int>& arr) {
+    int max_val = *max_element(arr.begin(), arr.end());
+
+    for (int exp = 1; max_val / exp > 0; exp *= 10) {
+        countingSort(arr, exp);
+    }
+}
+
+int main() {
+    vector<int> arr = {170, 45, 75, 90, 802, 24, 2, 66};
+    
+    radixSort(arr);
+    
+    cout << "Sorted array is:";
+    for (int num : arr) {
+        cout << " " << num;
+    }
+    cout << endl;
+
+    return 0;
+}
